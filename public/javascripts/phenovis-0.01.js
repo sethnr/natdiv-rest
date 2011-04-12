ec1bac40a0ea25807805084ad761fdb3516fc473
Seth Redmond <snr02@bio-iisrv1.bio.ic.ac.uk>
2011-04-12 18:50:43 +0100

    plot_scatter method

---
+++ public/javascripts/phenovis-0.01.js
@@ -0,0 +1,18 @@
+/* traverse (any) json object to find nodes of a particular type */
+function nodeFromArray(objects, catchString) {
+    for (var i = 0; i < objects.length; i++) {
+//	alert(i + " " + objects[i].value + " -> " + eval('objects[i].'+catchString));
+	if (eval('objects[i].'+catchString)) { return objects[i];}
+    }
+}
+
+
+function traverse(o,func) {
+    for (i in o) {
+        func.apply(this,[i,o[i]]);      
+        if (typeof(o[i])=="object") {
+                //going on step down in the object tree!!
+                traverse(o[i],func);
+        }
+    }
+}
