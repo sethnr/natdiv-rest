584095a7137af996669d904295b3b113f74f3c18
seth redmond <seth@SethBook.local>
2011-04-14 17:13:46 +0100

    automatic value-grabbing from json object added (use text-string notation to navigate through multiple levels of JSON object)
    Hell, and indeed, yes.

--- public/javascripts/phenovis-0.01.js
+++ public/javascripts/phenovis-0.01.js
@@ -44,7 +44,8 @@ function jitter(data, space) {
 	  
 	  /* add vertical jitter if overlaid dots on same x axis */
 	  if((d[_i].y == d[i].y) && (d[_i].xpos == d[i].xpos)) {
-	    theta = direction * Math.PI / 4;
+	    theta = (direction * (Math.PI / 4));
+	    //	    theta = Math.random()*(2*Math.PI);
 	    ytrans = diff * Math.sin(theta);
 	    xtrans = diff * Math.cos(theta);
 	  }
@@ -101,4 +102,117 @@ function jitter(data, space) {
   } while(overlaid && iterations < maxIterations);
   
   return d;
+}
+
+
+
+
+/* dot plot */
+
+function dotplot(data, w, h, xval, xselect, yval, yselect) {
+      /* Sizing and scales. */
+  var data, w, h, xval, xselect, yval, yselect;
+
+  /*
+
+  function xval(newXVal) {
+    xval = newXVal;
+  }
+
+  function yval(newYVal) {
+    yval = newYVal;
+  }
+
+  function xselect(newXSelect) {
+    xselect = newXSelect;
+  }
+
+  function yval(newYSelect) {
+    yselect = newYSelect;
+  }
+  */
+  
+  function findVals (valString) {
+    var objectPath = valString.split("->");
+    catchString = "data.experiments";
+    for (var i=0; i<objectPath.length;i++) {
+      var gets = objectPath[i].split(':');
+      var objectType = gets[0]; var predicate = gets[1];
+      //      alert(objectType+" -> "+predicate);
+      if(predicate != undefined)
+	catchString = catchString.concat(".collect(function(d) {return nodeFromArray(d.",objectType,",\"",predicate,"\")})");
+      else
+	catchString = catchString.concat(".collect(function(d) {return d.",objectType,"})");
+	//	catchString = catchString.concat(".pluck(",objectType,")");
+    }
+    var myVals = eval(catchString);
+    return myVals;
+  }
+
+
+  var myXvals = findVals("phenotypes:cvalue!=undefined->cvalue.name");
+  var myYvals = findVals("phenotypes:value>=0->value");
+
+  //  var xvals2 = data.experiments.collect(function(d) {return nodeFromArray(d.phenotypes,"cvalue!=undefined")}).collect(function(d) {return d.cvalue.name});
+
+
+      var xvals = data.experiments.collect(function(d) {return nodeFromArray(d.phenotypes, xselect).cvalue.name});
+      //      var xvals2 = data.pluck(experiments).collect(function(d) {return nodeFromArray(d.phenotypes,cvalue!=undefined)}).pluck(cvalue.name);
+      alert(xvals+"\n\n"+myXvals);
+
+
+      var yvals = data.experiments.collect(function(d) {return nodeFromArray(d.phenotypes, yselect).value});
+      alert(yvals+"\n\n"+myYvals);
+      
+      var x = pv.Scale.ordinal(xvals.uniq()).split(0, w),
+	y = pv.Scale.linear(0, yvals.max()).range(0, h).nice(),
+	s = x.range().band / 2,
+	c = pv.Colors.category10();
+
+      /* make data structure */
+      dataMap = data.experiments.map(function(d) {
+	  var xval = nodeFromArray(d.phenotypes, xselect).cvalue.name;
+	  var yval = nodeFromArray(d.phenotypes, yselect).value;
+	  return {x: xval, y: yval, xpos: x(xval), ypos: y(yval)}}).sortBy(function(d) {return Number(d.y)});
+      
+      /* The root panel. */
+      var vis = new pv.Panel()
+	.width(w)
+	.height(h)
+	.bottom(20)
+	.left(20)
+	.right(10)
+	.top(5);
+      
+      /* Y-axis and ticks. */
+      vis.add(pv.Rule)
+	.data(y.ticks())
+   	.bottom(y)
+	.strokeStyle(function(d) {return d ? "#eee" : "#000"})
+  	.anchor("left").add(pv.Label)
+	.text(y.tickFormat)
+	;
+      
+      /* X-axis and ticks. */
+      vis.add(pv.Rule)
+	.data(xvals.uniq())
+	.left(x)
+	.anchor("bottom")
+	.add(pv.Label)  	
+	;
+      
+      
+      /* The dot plot! */
+      panel = vis.add(pv.Panel)
+	.data(jitter(dataMap, 10))
+	//	 .data(dataMap)
+	;
+      
+      dots = panel.add(pv.Dot)
+	.bottom(function(d) {return d.ypos})
+	.left(function(d) {return d.xpos})
+	.strokeStyle(function(d) {return c(d.x)})
+	;
+      
+      return vis;   
 }
