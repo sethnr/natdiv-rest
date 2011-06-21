2edae1f77e2b3e71820bfd9c587ec828e07fc4a2
Seth Redmond <snr02@bio-iisrv1.bio.ic.ac.uk>
2011-06-21 14:19:10 +0100

    all routes now RESTfully flexible (object + action)

--- config/routes.rb
+++ config/routes.rb
@@ -20,42 +20,50 @@ ActionController::Routing::Routes.draw do |map|
     # experiment.resources :cvterms
   end
 
-   map.connect 'project/:id/head.:format',
-    :controller => "project",
-    :action => "head"
+   map.connect 'project/:id/:action.:format',
+    :controller => "project"
+
+#   map.connect 'project/:id/head.:format',
+#    :controller => "project",
+#    :action => "head"
  
-   map.connect 'project/:id/stocks.:format',
-    :controller => "project",
-    :action => "stocks"
+#   map.connect 'project/:id/stocks.:format',
+#    :controller => "project",
+#    :action => "stocks"
 
-   map.connect 'project/:id/experiments.:format',
-    :controller => "project",
-    :action => "experiments"
+#   map.connect 'project/:id/experiments.:format',
+#    :controller => "project",
+#    :action => "experiments"
 
+   map.connect 'stock/:id/:action.:format',
+    :controller => "stock"
 
-   map.connect 'stock/:id/head.:format',
-    :controller => "stock",
-    :action => "head"
+#   map.connect 'stock/:id/head.:format',
+#    :controller => "stock",
+#    :action => "head"
  
-   map.connect 'stock/:id/projects.:format',
-    :controller => "stock",
-    :action => "projects"
+#   map.connect 'stock/:id/projects.:format',
+#    :controller => "stock",
+#    :action => "projects"
  
-   map.connect 'stock/:id/experiments.:format',
-    :controller => "stock",
-    :action => "experiments"
+#   map.connect 'stock/:id/experiments.:format',
+#    :controller => "stock",
+#    :action => "experiments"
 
-   map.connect 'experiment/:id/head.:format',
-    :controller => "nd_experiment",
-    :action => "head"
+   map.connect 'experiment/:id/:action.:format',
+    :controller => "nd_experiment"
 
-   map.connect 'experiment/:id/stocks.:format',
-    :controller => "nd_experiment",
-    :action => "stocks"
+#   map.connect 'experiment/:id/head.:format',
+#    :controller => "nd_experiment",
+#    :action => "head"
 
-   map.connect 'experiment/:id/projects.:format',
-    :controller => "nd_experiment",
-    :action => "projects"
+#   map.connect 'experiment/:id/stocks.:format',
+#    :controller => "nd_experiment",
+#    :action => "stocks"
+
+#   map.connect 'experiment/:id/projects.:format',
+#    :controller => "nd_experiment",
+#    :action => "projects"
 
 
 
